af8e3434faaddfeb90c6b5bd93a16f5dec367921
Uzair Tariq <uzair0213@icloud.com>
2023-08-25 23:52:26 -0600

    Removed template content

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -57,7 +57,7 @@ module.exports = {
       transparent: "transparent",
     },
     fontFamily: {
-      mono: ["IBM Plex Mono", "monospace"],
+      mono: ["Fira Code", "JetBrains Mono", "monospace"],
     },
     // },
   },
